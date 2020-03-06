c30a16eaa224dc9689aad5c6e9f266274ccf240f
Igor Despot <Igor.Despot@endava.com>
2020-03-06 14:22:12 +0100

    Add gif search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function useGiphy(query) {
+
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let response = await fetch(
+          `https://api.giphy.com/v1/gifs/search?api_key=b55y9HDPY8YpA5WgpUoIkDKOhrBJ6ImT&q=${query}&limit=25&offset=0&rating=G&lang=en`
+        );
+        let json = await response.json();
+        setResults(json.data.map(item => { return item.images.preview.mp4 }));
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    if (query !== '') {
+      fetchData();
+    }
+  }, [query]);
+
+  return results;
+}
+
+export default function AsyncHooks() {
+
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('');
+
+  const results = useGiphy(query);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    setQuery(search);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Async React Hooks</h1>
+      <form onSubmit={ onSubmit }>
+        <input
+          value={ search }
+          onChange={ e => setSearch(e.target.value) }
+          placeholder="Search for Gifs!"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <br />
+      {
+        results.map(item => (
+          <video autoPlay loop key={ item } src={ item } />
+        ))
+      }
     </div>
   );
 }
-
-export default App;
